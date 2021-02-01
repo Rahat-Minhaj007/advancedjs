010a32ae497a5803808f87b35b22b75d9d5bdbae
Rahat-Minhaj007 <rahatminhaj444@gmail.com>
2021-02-02 02:04:09 +0600

    hi

---
+++ null-undefined.js
@@ -0,0 +1,21 @@
+// >>>>>>>>>>>>>undefined<<<<<<<<<<<<<<<
+// let pakhi;
+// console.log(pakhi);
+// function add(num1,num2){
+//     console.log(num1+num2);
+//     return
+// }
+
+// function add(num1,num2){
+//     console.log(num1,num2);
+// }
+
+// const result=add(10);
+// console.log(result);
+
+// const premik={name:"rahat",age:23};
+// console.log(premik.gf);
+
+// let fun =undefined;
+// console.log(fun);
+// undefined is falsy or negative;
---
+++ trueFalse.js
@@ -0,0 +1,28 @@
+// falsy:
+
+// 0
+// undefined
+// null
+// NaN
+// ""
+
+// truthy
+
+// "0"
+// " "
+// []
+
+
+
+// const name=" ";
+
+let name=12;
+console.log(name);
+if (name || name==0) {
+    console.log("condition is true")
+    
+}
+
+else{
+    console.log("condition is false")
+}
